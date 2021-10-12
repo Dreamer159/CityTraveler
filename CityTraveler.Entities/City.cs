6428241642c9f3bbb7cd91a2572c81f76d1661c2
DESKTOP-T4OO239\USER_PC_0381 <olukianova86@gmail.com>
2021-10-12 11:56:21 +0300

    Add classes City, User, enum UserRole

---
+++ CityTraveler.Entities/City.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityTraveler.Entities
+{
+    public class City
+    {
+        public IEnumerable<string> Institution { get; set; } // change IEnumerable 
+        public IEnumerable<string> Events { get; set; } // change IEnumerable 
+        public IEnumerable<string> SightSeengs { get; set; } // change IEnumerable 
+    }
+}
