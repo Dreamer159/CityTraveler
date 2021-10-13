32667e626ffa61c9229ada232a7633eb88df7224
njqdj <sergejminaev18@gmail.com>
2021-10-13 13:15:00 +0300

    Created interface ItripService wit some methods

---
+++ CityTraveler.Infrastructure/Interfaces/Service/ITripService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityTraveler.Infrastructure.Interfaces.Service
+{
+    interface ITripService:IServiceMetadata
+    {
+        public bool AddNewTrip(ITrip newTrip);
+
+        public bool DeleteTrip(ITrip trip);
+
+        public IEnumerable<ITrip> GetTrips(IEnumerable<Guid> guids);
+
+        public ITrip GetTripById(Guid tripId);
+
+        public Dictionary<Guid, Guid> GetTripByLandscap(Guid landscapeId, Guid tripId);
+
+        public Dictionary<Guid, Guid> GetTripByRating(Guid ratingId, Guid tripId);
+
+
+
+    }
+}
--- CityTraveler.Infrastructure/Interfaces/Service/IUserManagementService.cs
+++ CityTraveler.Infrastructure/Interfaces/Service/IUserManagementService.cs
@@ -16,6 +16,8 @@ namespace CityTraveler.Infrastructure.Interfaces
         public IEnumerable<IUser> GetUsers(IEnumerable<Guid> guids);
         public IUser GetUserById(Guid userId);
         public IUser GetUserByEmail(string email);
+
+        
     }   
 }
 
