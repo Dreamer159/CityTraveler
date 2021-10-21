e7b1c0252380179dc9d6ef656b17ecd141e7adb8
Kate <kateryna.kondratiuk@ukma.edu.ua>
2021-10-21 11:39:47 +0300

    finished first version of city architecture and social media services. Pretty sure a lot of changes are coming...............................................

--- CityTraveler.Services/Interfaces/ICityArchitectureService.cs
+++ CityTraveler.Services/Interfaces/ICityArchitectureService.cs
@@ -14,20 +14,19 @@ namespace CityTraveler.Services.Interfaces
         public Task<bool> UpdateCityEntertainment(EntertaimentModel institution);
         public Task<bool> RemoveEntertainmentById(Guid objectId);
         public Task<bool> AddStreet(StreetModel street);
-        //id of object? 
         public Task<bool> UpdateStreet(StreetModel street);
         public Task<bool> RemoveStreet(Guid streetId);
-        //longtitude, latitude method
-        //add task to all methods
-        public StreetModel FindStreetByCoordinates(Guid coordID);
-        public AddressModel FindAddressByCoordinates(Guid coordID);
-        public IEnumerable<EntertaimentModel> FindEntertainmentByStreet(Guid streetId);
-        public IEnumerable<EntertaimentModel> FindEntertainmentByCoordinate(Guid streetId);
-        public IEnumerable<EntertaimentModel> FindEntertainmentByAddress(Guid streetId);
-        public AddressModel FindAddressByStreetHouse(Guid streetId, string houseNum);
-        public IEnumerable<AddressModel> FindAddressByHouse(string houseNum);
-        public IEnumerable<EntertaimentModel> FindEntertainmentByStreetTitle(string streetTitle);
-        public Task<bool> BuildCityMap();
+        public Task<StreetModel> FindStreetByCoordinates(Guid coordID);
+        public Task<StreetModel> FindStreetByCoordinates(double longtitude, double latitude);
+        public Task<AddressModel> FindAddressByCoordinates(Guid coordID);
+        public Task<AddressModel> FindAddressByCoordinates(double longtitude, double latitude);
+        //TODO
+        public Task<List<EntertaimentModel>> FindEntertainmentByStreet(Guid streetId);
+        public Task<IEnumerable<EntertaimentModel>> FindEntertainmentByCoordinate(Guid streetId);
+        public Task<IEnumerable<EntertaimentModel>> FindEntertainmentByAddress(Guid streetId);
+        public Task<AddressModel> FindAddressByStreetHouse(Guid streetId, string houseNum);
+        public Task<IEnumerable<AddressModel>> FindAddressByHouse(string houseNum);
+        public Task<IEnumerable<EntertaimentModel>> FindEntertainmentByStreetTitle(string streetTitle);
         public Task<bool> ValidateCityMap();
     }   
 }
