f9d1041102dff17f786c2a676777f631d9c437e0
njqdj <sergejminaev18@gmail.com>
2021-10-26 12:45:54 +0300

    remove tabs

--- CityTraveler.Services/Interfaces/IImageService.cs
+++ CityTraveler.Services/Interfaces/IImageService.cs
@@ -9,17 +9,11 @@ namespace CityTraveler.Services.Interfaces
 {
     public interface IImageService<T> where T:ImageModel, IServiceMetadata
     {
-
         public Task<bool> AddNewImage(T image);
         public Task<bool> DeleteImage(Guid imageId);
-
         public IEnumerable<T> GetImages(int skip = 0, int take = 7);
         public Task<T> GetImageByIdAsync(Guid imageId);
-
         public Task<bool> AddAvatarToUserProfile(string src);
         public Task<bool> UpdatAvatarForUserProfile(string src,  Guid userId);
-        //public Task<bool> DeleteAvatarFromUserProfile(string src);
-
-
     }
 }
